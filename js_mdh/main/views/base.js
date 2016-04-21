1bd4d60b09295aede9016b79824aa957881389d5
tianjian <1012649425@qq.com>
2016-04-21 14:26:22 +0800

    bug修改

--- js_mdh/main/views/base.js
+++ js_mdh/main/views/base.js
@@ -18,16 +18,16 @@
       }
 
 
-  $('.nav ul li').hover(function() {
-    $(this).find('.innav').stop().slideDown(300);
+  $('.nav>ul>li').hover(function() {
+    $(this).find('.innav').slideDown(300);
   }, function() {
-    $(this).find('.innav').stop().slideUp(300);
+    $(this).find('.innav').slideUp(300);
   });
 
-  $('.innav ul li').mouseover(function(event) {
+  $('.innav>ul>li').mouseover(function(event) {
     var n=$(this).index();
     $(this).addClass('current').siblings().removeClass('current');
-    $('.innav ol li').eq(n).css('display', 'block').siblings().css('display', 'none');;
+    $('.innav>ol>li').eq(n).css('display', 'block').siblings().css('display', 'none');;
   });
 
  
